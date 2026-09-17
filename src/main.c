5645855412d738d07ccde6c779893b76de186785
ebrfoot317-a11y <ebrfoot317@gmail.com>
2026-09-18 00:58:55 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 #include <stddef.h>
 #include <string.h>
 
-const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "NES";
+const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "Super Mario Bros";
 const uint32_t eadk_api_level  __attribute__((section(".rodata.eadk_api_level"))) = 0;
 
 // Newlib's implementation of rand allocates and raises
